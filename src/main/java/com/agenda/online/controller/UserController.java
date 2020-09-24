3c4ff8e156164426229ad685258e5a0175548680
MoisesRabelo <moisesramo48@gmail.com>
2020-09-24 00:01:18 -0300

    controller básico do usuário(página de entrada)

---
+++ src/main/java/com/agenda/online/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.agenda.online.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController
+{
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
