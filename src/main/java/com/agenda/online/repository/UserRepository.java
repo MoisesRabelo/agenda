e95402d54c3aa35021782414efa90f2a2738b9c6
MoisesRabelo <moisesramo48@gmail.com>
2020-09-24 00:06:56 -0300

    repository basico do Usuario

---
+++ src/main/java/com/agenda/online/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.agenda.online.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agenda.online.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+	User findByEmail(String email);
+	
+	//List<User> findAllById(Iterable<Long> id);
+}
